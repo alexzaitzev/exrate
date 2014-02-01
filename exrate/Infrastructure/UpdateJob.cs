43692ee6a72abaf41171bcbc3d7fd5567bed9b83
Alex Zaitsev <info@alexzaitzev.pro>
2014-02-01 23:28:16 +0400

    Delete config files

--- exrate/Infrastructure/UpdateJob.cs
+++ exrate/Infrastructure/UpdateJob.cs
@@ -5,32 +5,32 @@ using NLog;
 
 namespace Exrate.Infrastructure
 {
-    [DisallowConcurrentExecution]
-    public class UpdateJob : IJob
-    {
-        private readonly IRateService _rateService;
-        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+	[DisallowConcurrentExecution]
+	public class UpdateJob : IJob
+	{
+		private readonly IRateService _rateService;
+		private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-        public UpdateJob()
-        {
-            _rateService = new RateService(new RateRepository());
-        }
+		public UpdateJob()
+		{
+			_rateService = new RateService(new RateRepository());
+		}
 
-        public virtual void Execute(IJobExecutionContext context)
-        {
-            var dataMap = context.JobDetail.JobDataMap;
-            var city = (Enums.Cities)dataMap.Get(Job.City);
-            switch (city)
-            {
-                case Enums.Cities.Krsk:
-                    _logger.Debug("Krsk banks was updated");
-                    _rateService.UpdateKrskRates();
-                    break;
-                case Enums.Cities.Msc:
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+		public virtual void Execute(IJobExecutionContext context)
+		{
+			var dataMap = context.JobDetail.JobDataMap;
+			var city = (Enums.Cities)dataMap.Get(Job.City);
+			switch (city)
+			{
+				case Enums.Cities.Krsk:
+					_logger.Debug("Krsk banks was updated");
+					_rateService.UpdateKrskRates();
+					break;
+				case Enums.Cities.Msc:
+					break;
+				default:
+					break;
+			}
+		}
+	}
 }
